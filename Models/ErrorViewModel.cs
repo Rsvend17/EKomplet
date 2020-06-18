3f0df2944e626da3a87544035ee5348a056caf76
Rsvend17 <34129384+Rsvend17@users.noreply.github.com>
2020-06-18 21:51:17 +0200

    added dot.net core application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EKomplet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
